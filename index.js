05cf69f3e30f839852200851ed03d5fbb4933699
unknown <forkan.saadaoui@gmail.com>
2023-05-22 23:27:03 +0100

    get book by id

--- index.js
+++ index.js
@@ -14,11 +14,26 @@ const app = express();
 
 app.get("/", (req,res) => {
     res.send("Welcome Node Js")
-}) /*The client send a request to the server */
+}) /*The client get a request to the server */
 
 app.get("/api/books", (req,res) => {
     res.json(books)
 })
+// Get Book Method By Id
+app.get("/api/books/:id", (req,res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id))
+    if (book) {
+        res.status(200).json(book)
+        /*
+            status 200: status response code from a server for successful HTTP requests from a client (browser). 
+            For a web page, it indicates that its HTML code can be loaded successfully. 
+            book is exist
+        */
+    } else {
+        res.status(404).json({message: "Book Not Found"})
+    }
+})
+
 
 
 
